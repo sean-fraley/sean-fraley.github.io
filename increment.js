d9a1588c4341b7bb5b45cbdf0ae28b92fa4c3954
sean-fraley <fraley.saen@gmail.com>
2015-01-20 22:12:12 -0500

    Removed set_cookie and read_cookie functions
    Removed *_cookie functions from increment.js in response to learning
    jQuery.cookie

--- increment.js
+++ increment.js
@@ -1,26 +1,4 @@
 //////////////////////
-// COOKIES & SAVING //
-//////////////////////
-function set_cookie ( cookie_name, cookie_value, lifespan_in_days, valid_domain )
-{
-  // http://www.thesitewizard.com/javascripts/cookies.shtml
-  var domain_string = valid_domain ? ("; domain=" + valid_domain) : '' ;
-  document.cookie = cookie_name + "=" + encodeURIComponent( cookie_value ) +
-      "; max-age=" + 60 * 60 * 24 * lifespan_in_days +
-      "; path=/" + domain_string ;
-}
-function get_cookie ( cookie_name )
-{
-  // http://www.thesitewizard.com/javascripts/cookies.shtml
-  var cookie_string = document.cookie ;
-  if (cookie_string.length != 0) {
-    var cookie_value = cookie_string.match ( '(^|;)[\s]*' + cookie_name + '=([^;]*)' );
-    return decodeURIComponent ( cookie_value[2] ) ;
-  }
-  return '' ;
-}
-
-//////////////////////
 // GLOBAL VARIABLES //
 //////////////////////
 var FPS = 10;
